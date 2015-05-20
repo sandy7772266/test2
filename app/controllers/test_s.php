cbc268dcb97274f5d36157fa090d9e27d4b6233c
sandy7772266 <sandy@mail.ttcps.ntpc.edu.tw>
2015-05-20 11:55:24 +0800

    init

---
+++ app/controllers/test_s.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	
+</head>
+<body>
+
+<?php $value = Session::get('data_s');
+echo $value;  echo '3';?>
+	<div class="welcome">
+		<a href="http://laravel.com" title="Laravel PHP Framework">
+		<h1>Laravel + LightOpenID 範例 for 新北市</h1>
+		
+		<p><a href="login/openid" style="font-size:24px;">OpenID 登入</a></p>
+
+	</div>
+</body>
+</html>
